8a42671d1e3d2ce5fd785ea8b3251843e3489da3
d-german <facmet107@gmail.com>
2018-11-23 12:07:52 -0600

    made some changes

---
+++ AsyncTests/AsyncTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsyncTests
+{
+    [TestClass]
+    public class AsyncTests
+    {
+        public static int Id = 1000;
+
+        // Recursively calculates Fibonacci numbers
+        public static long Fibonacci(long n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public Task<long> FibonacciAsync(long n)
+        {
+            return Task.Run(() => Fibonacci(n));
+        }
+
+        public static string LongOperation(string value, int waitTime = 1)
+        {
+            Thread.Sleep(waitTime * 1000);
+            return value.ToUpper();
+        }
+
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            long answer = 0;
+            await Task.Run(() => answer = Fibonacci(20));
+            Assert.AreEqual(answer, 6765);
+        }
+
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            Assert.AreEqual(await FibonacciAsync(18), 2584);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(Fibonacci(20), 6765);
+        }
+
+    }
+}
---
+++ AsyncTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AsyncTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AsyncTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6aefd533-e36f-4a48-b539-df1e54cabc8e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
